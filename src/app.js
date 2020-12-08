16ad6e22792a5e69b5106a5032e72dae7ebb711f
jooheeLee <joooohee@kakao.com>
2020-12-08 18:12:19 +0900

    Fix static path to app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import express from "express";
 import morgan from "morgan";
 import path from "path";
 
-import { webPoolPromise } from "./db.js";
+// import { webPoolPromise } from "./db.js";
 
 const home = async (req, res) => {
   try {
-    const pool = await webPoolPromise;
+    // const pool = await webPoolPromise;
     // const { recordset } = await pool
     //   .request()
     //   .query(`SELECT USER_ID, USER_NAME FROM [USER] WHERE USER_ID='${id}' AND USER_PASSWORD='${password}'`);
@@ -19,8 +19,8 @@ const home = async (req, res) => {
 const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "css")));
-app.use(express.static(path.join(__dirname, "js")));
+app.use(express.static(path.join(__dirname, "static")));
+// app.use(express.static(path.join(__dirname, "js")));
 app.use(morgan("dev"));
 app.use("/", home);
 
